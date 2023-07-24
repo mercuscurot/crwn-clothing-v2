a8669d4a0282070ae5f42aa610b27572d97ac6a7
paolo.drovetti <paolo.drovetti@ogilvy.com>
2023-07-24 17:12:21 +0200

    first commit

---
+++ crwn-clothing/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import './category-item.style.scss'
+
+const CategoryItem = ({ category }) => {
+
+    const { imageUrl, title } = category
+
+    return (
+        <div className="category-container">
+            {/*
+                    <div className="backgronud-image" style={{backgroundImage: `url(${imageUrl})`,}}></div>
+                    */}
+            <div className='background-image' style={{backgroundImage: `url(${imageUrl})`,}}></div>
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop now</p>
+            </div>
+        </div>
+    )
+
+}
+export default CategoryItem
